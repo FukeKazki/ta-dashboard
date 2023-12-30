6b7c83a48372d0cbbe2a0eb12f6df20ec4c6d862
FukeKazki <kazkichi0906@gmail.com>
2023-12-30 22:29:16 +0900

    APIサーバーが立つところ

--- packages/api/src/main.go
+++ packages/api/src/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-  log.Println("Starting TaDashboard API hello")
+	log.Println("Starting TaDashboard API hello")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":4000"))
 }
