a96617abfe3cb96e50bc78893c0bd102b2407ab4
FukeKazki <kazkichi0906@gmail.com>
2024-01-07 15:29:13 +0900

    ユーザー作成時に対応

--- packages/api/src/infrastracture/user.go
+++ packages/api/src/infrastracture/user.go
@@ -16,22 +16,12 @@ func NewUserRepository(conn *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) Create(user *model.User) (*model.User, error) {
-	// insert public_profile
-	publicProfile, err := r.Connection.Exec("INSERT INTO public_profile (NAME) VALUES (?)", user.Name)
-	if err != nil {
-		return nil, err
-	}
-	// get public_profile_id
-	publicProfileID, err := publicProfile.LastInsertId()
-	if err != nil {
-		return nil, err
-	}
 	// binary uuid   id BINARY(16) NOT NULL DEFAULT (UUID_TO_BIN(UUID(), 1)),
 	uu := uuid.New()
 	userID, err := uu.MarshalBinary()
 
 	// insert user
-	_, err = r.Connection.Exec("INSERT INTO user (ID, PUBLIC_PROFILE_ID, NAME, PASSWORD) VALUES (?, ?, ?, ?)", userID, publicProfileID, user.Name, user.Password)
+	_, err = r.Connection.Exec("INSERT INTO user (ID, NAME, PASSWORD) VALUES (?, ?, ?, ?)", userID, user.Name, user.Password)
 	if err != nil {
 		return nil, err
 	}
